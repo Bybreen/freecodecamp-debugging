405d2a5a6d5a479508d6ffaca9c49acd618e35f1
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-26 12:05:54 +0100

    Catch Mixed Usage of Single and Double Quotes
    JavaScript allows the use of both single ('') and double ("") quotes to declare a string. Deciding which one to use generally comes down to personal preference, with some exceptions.
    
    Having two choices is great when a string has contractions or another piece of text that's in quotes. Just be careful that you don't close the string too early, which causes a syntax error.
    
    Here are some examples of mixing quotes:
    
    // These are correct:
    const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
    const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
    // This is incorrect:
    const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it.';
    Of course, it is okay to use only one style of quotes. You can escape the quotes inside the string by using the backslash (\) escape character:
    
    // Correct use of same quotes:
    const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';

---
+++ Knowing-Mixed-Usage-of-Single-and-Double-Quotes.js
@@ -0,0 +1,5 @@
+// Fix the string so it either uses different quotes for the href value, or escape the existing ones. 
+//Keep the double quote marks around the entire string.
+
+let innerHTML = "<p>Click here to <a href='#Home>return Home</a></p>";
+console.log(innerHTML);
