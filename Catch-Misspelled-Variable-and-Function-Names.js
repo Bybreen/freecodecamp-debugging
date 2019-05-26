07ba1b8eebd185a5a8b0c23b7eeafc84cd499eb6
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-26 11:39:35 +0100

    Misspelled Variable and Function Names
    he console.log() and typeof methods are the two primary ways to check intermediate values and types of program output. Now it's time to get into the common forms that bugs take. One syntax-level issue that fast typers can commiserate with is the humble spelling error.
    
    Transposed, missing, or mis-capitalized characters in a variable or function name will have the browser looking for an object that doesn't exist - and complain in the form of a reference error. JavaScript variable and function names are case-sensitive.

---
+++ Catch-Misspelled-Variable-and-Function-Names.js
@@ -0,0 +1,7 @@
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works.
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+
+console.log(`net working capital is: ${`netWorkingCapital}`);
